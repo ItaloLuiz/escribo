c1601d1de36f9748ae564a4b85ef8d41c1eb88af
Italo Luiz <italo.luis.s@gmail.com>
2021-01-09 11:47:20 -0300

    refatorando o código

--- exemplo.js
+++ exemplo.js
@@ -1,7 +1,53 @@
 let jogo = new CobrasEscadas();
+let jogoEstatico = false;
 
-let inicio = jogo.jogar(1,2);
-//let inicio = jogo.jogar(primeiroDado,segundoDado);
+if(jogoEstatico){
+    jogo.jogar(1,2);
+}else{
+    jogo.jogar(jogo.jogarDado(), jogo.jogarDado());
+}
+
+function resetarJogo() {
+    localStorage.setItem('posicaoJogadorA', 0);
+    localStorage.setItem('posicaoJogadorB', 0);
+    document.location.reload(true);
+}
+
+function tabela() {
+
+    function mostrarJogador(casaAtual) {
+        let posicaoPogadorA = localStorage.getItem('posicaoJogadorA');
+        let posicaoPogadorB = localStorage.getItem('posicaoJogadorB');
+
+        if (casaAtual == posicaoPogadorA) {
+            return `<div class="jogadorA"></div>`;
+        }
+        else if (casaAtual == posicaoPogadorB) {
+            return `<div class="jogadorB"></div>`;
+        }
+        else if ((casaAtual == posicaoPogadorA) && (casaAtual == posicaoPogadorB)) {
+            return `<div class="jogadorA"></div><div class="jogadorB"></div>`;
+        }
+        else {
+            return '<div class="jogadorN"></div>';
+        }
+    }
+
+
+    let template = `<div id="tabuleiro">`;
+    for (let i = 1; i <= 100; i++) {
+        template += `<div class="casa" id="id_casa-${i}">
+           ${mostrarJogador(i)}
+          <h4>${i}</h4>
+        </div>`;
+
+    }
+    template += `</div>`;
+    if (document.getElementById('tabela')) {
+        document.getElementById('tabela').innerHTML = template;
+    }
+}
+tabela('tabela');
 
 
 
